2212a69f6dec1c7a875e606aaa8c54540dd1b2a5
cyyu <404613291@qq.com>
2026-04-16 14:18:21 +0800

    feat: update start script to use variable for biyachaind path and enable optimistic execution

--- start.sh
+++ start.sh
@@ -4,6 +4,11 @@ rm -rf chain-stresser-deploy
 # template: evm  
 # app.evm.toml.tpl | config.prod.toml.tpl | genesis.evm.json.tpl | 
 NODE_NUM=4
+BIYACHIAND="/home/ubuntu/biyachain/biyachain-core/bin/biyachaind"
+
+log_level="info"
+optimistic_execution_enabled=true
+
 chain-stresser generate --accounts-num 1000 --validators $NODE_NUM --sentries 0 --instances 1 --prod --native
 
 mkdir -p node-log/
@@ -12,6 +17,9 @@ VALIDATOR_DIR="./chain-stresser-deploy/validators"
 LOG_DIR="./node-log"
 
 for i in $(seq 0 $(($NODE_NUM - 1))); do
-    biyachaind --home="$VALIDATOR_DIR/$i" start > "$LOG_DIR/node$i.log" 2>&1 &
+    $BIYACHIAND --home="$VALIDATOR_DIR/$i" \
+        --optimistic-execution-enabled=$optimistic_execution_enabled \
+        --log-level=$log_level \
+    start > "$LOG_DIR/node$i.log" 2>&1 &
     echo $! > "$LOG_DIR/pid$i.pid"
 done
